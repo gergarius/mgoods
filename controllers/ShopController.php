b17373b93cc224a7c42570d57a7ccd70f4ca8392
Romario <uca8@mail.ru>
2015-07-30 21:44:07 +0300

    verstka

---
+++ controllers/ShopController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace app\controllers;
+
+use app\components\ShopData;
+use Yii;
+
+class ShopController extends \yii\web\Controller
+{
+    public function actionIndex()
+    {
+        $shopName = "pingvinpc.ru";
+       // $brand = ["[1]"=>"canon", "[2]"=>"asus"];
+        $brand = [];
+        $data = ShopData::getDataShop($shopName, $brand);
+
+        if (!$data) {
+            throw new BadRequestHttpException(Yii::t('app', 'Specified record is not found'), 404);
+        }
+
+
+        Yii::$app->view->registerMetaTag([
+            'name'    => 'description',
+            'content' => $data->shop->metadescription
+        ]);
+        Yii::$app->view->registerMetaTag([
+            'name'    => 'keywords',
+            'content' => $data->shop->metakeywords
+        ]);
+
+
+
+        return $this->render('index', ['data'=>$data]);
+
+    }
+
+}
